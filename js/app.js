b3559449066439f74912542ee6cc136f80553736
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-07-17 08:25:15 -0400

    Added functionality for: Calculating calories consumed, burned, remaining, limit and gain/loss

--- js/app.js
+++ js/app.js
@@ -1,18 +1,77 @@
 class CalorieTracker {
+
+        // With private properties we do not need a constructor anymore but if we would like to trigger some private methods we have to use this.
+        constructor(){
+            this.#displayCaloriesTotal();
+            this.#displayCaloriesLimit(); 
+            this.#displayCaloriesConsumed();
+            this.#displayCaloriesBurned();
+            this.#displayCaloriesRemaining();
+        }
+
         #calorieLimit = 2000; 
         #totalCalories = 0;
         #meals = [];
         #workouts = [];
 
+        // Public Methods/API //
+
         addMeal(meal) {
             this.#meals.push(meal);
             this.#totalCalories += meal.calories;
+            this.#render();
         }
 
         addWorkout(workout) {
             this.#workouts.push(workout);
-            this.#totalCalories += workout.calories;
+            this.#totalCalories -= workout.calories;
+            this.#render();
         }
+        
+        // Private Methods //
+
+        #displayCaloriesTotal(){
+            const totalCaloriesEl = document.getElementById('calories-total');
+            totalCaloriesEl.innerHTML = this.#totalCalories;
+        }
+
+        #displayCaloriesLimit(){
+            const calorieLimitEl = document.getElementById('calories-limit');
+            calorieLimitEl.innerHTML = this.#calorieLimit;
+        }
+
+        #displayCaloriesConsumed(){
+            const caloriesConsumedEl = document.getElementById('calories-consumed');
+
+            const consumed = this.#meals.reduce((total, meal) => total + meal.calories, 0);
+
+            caloriesConsumedEl.innerHTML = consumed;
+        }
+
+        #displayCaloriesBurned(){
+            const caloriesBurnedEl = document.getElementById('calories-burned');
+
+            const burned = this.#workouts.reduce((total, workout) => total + workout.calories, 0);
+
+            caloriesBurnedEl.innerHTML = burned;
+        }
+
+        #displayCaloriesRemaining(){
+            const caloriesRemainingEl = document.getElementById('calories-remaining');
+
+            const remaining = this.#calorieLimit - this.#totalCalories;
+
+            caloriesRemainingEl.innerHTML = remaining;
+        }
+
+        // We have to do the rendering everytime we change the dom so we call this method //
+        #render(){
+            this.#displayCaloriesTotal();
+            this.#displayCaloriesConsumed();
+            this.#displayCaloriesBurned();
+            this.#displayCaloriesRemaining();
+        }
+
     }
 
 class Meal {
@@ -34,15 +93,13 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
 
 tracker.addMeal(breakfast);
-const run = new Workout('Morning Run', 300);
+tracker.addMeal(lunch);
+const run = new Workout('Morning Run', 320);
 
 tracker.addWorkout(run);
-
-// Test Git Username
-
-// Test Email
 
 
 
