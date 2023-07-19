536cdd07ee9287816bd0a889120b21ca7782b46f
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-07-19 08:10:32 -0400

    Added functionality for filtering workouts and meals. Also added Reset Button functionality

--- js/app.js
+++ js/app.js
@@ -52,6 +52,13 @@ class CalorieTracker {
                 this.#render();
             }
         }
+
+        reset(){
+            this.#totalCalories = 0;
+            this.#meals = [];
+            this.#workouts = [];
+            this.#render();
+        }
         
         // Private Methods //
 
@@ -191,6 +198,9 @@ class App {
         document.getElementById('workout-form').addEventListener('submit', this.#newItem.bind(this, 'workout'));
         document.getElementById('meal-items').addEventListener('click', this.#removeItem.bind(this, 'meal'));
         document.getElementById('workout-items').addEventListener('click', this.#removeItem.bind(this, 'workout'));
+        document.getElementById('filter-meals').addEventListener('keyup', this.#filterItems.bind(this, 'meal'));
+        document.getElementById('filter-workouts').addEventListener('keyup', this.#filterItems.bind(this, 'workout'));
+        document.getElementById('reset').addEventListener('click', this.#reset.bind(this));
     }
 
     #tracker = new CalorieTracker();
@@ -243,6 +253,28 @@ class App {
                 e.target.closest('.card').remove();
             }
         }
+    }
+
+    #filterItems(type, e){
+        const text = e.target.value.toLowerCase();
+        
+        document.querySelectorAll(`#${type}-items .card`).forEach(item => {
+            const name = item.firstElementChild.firstElementChild.textContent;
+
+            if(name.toLowerCase().indexOf(text) !== -1){
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        })
+    }
+
+    #reset(){
+        this.#tracker.reset();
+        document.getElementById('meal-items').innerHTML = '';
+        document.getElementById('workout-items').innerHTML = '';
+        document.getElementById('filter-meals').value = '';
+        document.getElementById('filter-workouts').value = '';
     }
 
 }
